6c82c614e2d71f8103cc87477f71919e9bacfc15
labhk <labh.k2003@gmail.com>
2023-07-21 20:34:25 +0530

    mobile responsive design and CN version added

--- src/pages/MegaIdeaHack.jsx
+++ src/pages/MegaIdeaHack.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import ContactUs from '../components/ContactUs';
 import Styles from './MegaIdeaHack.module.css';
 import { useTranslation } from 'react-i18next';
+import FAQ from '../components/FAQ';
 
 function MegaIdeaHack() {
     const { t, i18n } = useTranslation();
@@ -14,6 +15,9 @@ function MegaIdeaHack() {
 
     const lng = navigator.language;
 
+    const [visible, setVisible] = useState(false);
+
+
     return (
     <>
         <Navbar/>
@@ -21,48 +25,81 @@ function MegaIdeaHack() {
         <div className={Styles.snakeDiv}>
             <div className={Styles.snake}>
                 <div className={Styles.snakeText1}>
-                Want to meet people with similar interests but different majors and interests?
+                {t('idea.snaketext1')} 
                 </div>
                 <div className={Styles.snakeImg1}>
-                    <img src="/line1.svg" alt="snake" />
+                <img src="/line1.svg" alt="snake" />
                 </div>
                 <div className={Styles.snakeText2}>
-                With the fields of reinforcement learning, GPT4, large language models, and AGI evolving so rapidly, are you curious about how you can make your mark in the future?
+                {t('idea.snaketext2')} 
                 </div>
                 <div className={Styles.snakeImg2}>
                 <img src="/line2.svg" alt="snake" />
                 </div>
                 <div className={Styles.snakeText3}>
-                Want to turn your quest and sense of purpose into tangible action?
+                {t('idea.snaketext3')}
                 </div>
                 <div className={Styles.snakeImg3}>
                 <img src="/line3.svg" alt="snake" />
                 </div>
                 <div className={Styles.snakeText4}>
-                Want to use your knowledge and skills to make a positive difference in society? 
+                {t('idea.snaketext4')}
                 </div>
                 <div className={Styles.snakeImg4}>
                 <img src="/line4.svg" alt="snake" />
                 </div>
             </div>
-            <div className={Styles.megaIdeaHackDiv}>
-                <div className={Styles.megaIdeaHack}>
-                    <div className={Styles.megaIdeaHackHead}>MEGA IDEAHACK</div>
-                    <div className={Styles.megaIdeaHackText}>
-                        <div>2023.7.22-25</div>
-                        <div>Shanghai ,</div>
-                        <div>China</div>
-                    </div>
+
+            <div className={Styles.snakeMobile}>
+                <div className={Styles.snakeText}>
+                {t('idea.snaketext1')} 
                 </div>
-                <div className={Styles.megaIdeaHackImg}>
-                    <img src="/landslide.svg" alt="slide" />
+                <div className={Styles.snakeImg}>
+                <img src="/snake1.svg" alt="snake" />
+                </div>
+                <div className={Styles.snakeText}>
+                {t('idea.snaketext2')} 
+                </div>
+                <div className={Styles.snakeImg}>
+                <img src="/snake2.svg" alt="snake" />
+                </div>
+                <div className={Styles.snakeText}>
+                {t('idea.snaketext3')} 
+                </div>
+                <div className={Styles.snakeImg}>
+                <img src="/snake1.svg" alt="snake" />
+                </div>
+                <div className={Styles.snakeText}>
+                {t('idea.snaketext4')} 
+                </div>
+                <div className={Styles.snakeImg}>
+                <img src="/snake2.svg" alt="snake" />
                 </div>
             </div>
-        </div>
 
-        <div className={Styles.goals}>
+            <div className={Styles.megaIdeaHackDiv}>
+                <div className={Styles.megaIdeaHack}>
+                <div className={Styles.megaIdeaHackHead}>
+                MEGA IDEAHACK 
+                </div>
+                <div className={Styles.megaIdeaHackText}>
+                    <div>2023.7.22-25</div> 
+                    <div>{t('idea.city1')} </div> 
+                    <div>
+                    {t('idea.city2')}
+                    </div> 
+                </div>
+                </div>
+                <div className={Styles.megaIdeaHackImg}>
+                <img src="/landslide.svg" alt="slide" />
+                </div>
+            </div>
+            </div>
+
+
+            <div className={Styles.goals}>
             <div className={Styles.goalsText}>
-            MEGA China's new project will focus on three to four of the 17 Sustainable Development Goals (SDGS) of the United Nations, looking for realistic issues related to them.
+                {t('idea.goaltext')}
             </div>
             <div className={Styles.goalsImg}>
                 <img src="/goals.svg" alt="goals" />
@@ -70,43 +107,84 @@ function MegaIdeaHack() {
             <div className={Styles.goalsTag}>
             en_*具体问题和方向将会在后续推文放出，敬请期待。
             </div>
-        </div>
+            </div>
 
-        <div className={Styles.circleDiv}>
+            <div className={Styles.circleDiv}>
             <div className={Styles.yellow}>
                 <div className={Styles.circle}></div>
             </div>
             <div className={Styles.circleText}>
-                <div>You will team up with other participants and, under the guidance of well-known university teaching assistants and industry leaders, jointly design solutions, create related works and develop landing projects in three days from the United Nations Sustainable Development Goals. It can take the form of a charity campaign, a research proposal, a web application, or a work of art.</div>
-                <div>At the end of the event, there will be a result-sharing display where your projects will be reviewed and awarded by experts. The proposal produced by the winning group will have the opportunity to land directly, and the members of the group will also receive generous rewards, including but not limited to financial and technical support, and related internship opportunities.</div>
+                <div>
+                {t('idea.circletext1')}
+                </div>
+                <div>
+                {t('idea.circletext2')}
+                </div>
             </div>
             <div className={Styles.circleCards}>
                 <img src="/cards.svg" alt="cards" />
             </div>
-        </div>
+            </div>
 
-        <div className={Styles.college_images}>
-            <div className={Styles.colleges}>*MEGA College teaching assistants come from but are not limited to</div>
-        <div className={Styles.college_image}>
-          <img src="/Group 46.svg" alt="College List" />
-        </div>
-        </div>
+            <div className={Styles.college_images}>
+            <div className={Styles.colleges}>
+                {t('idea.colleges')}
+            </div>
+            <div className={Styles.college_image}>
+                <img src="/Group 46.svg" alt="College List" />
+            </div>
+            </div>
 
-        
-        <div className={Styles.coffee_chat}>
+
+       
+            <div className={Styles.coffee_chat}>
             <div className={Styles.coffee_shapes}>
                 <div className={Styles.coffee_shape}>
-                    <img src= "/Vector (1).svg" alt = "Star"/>
-                    <img src= "/Vector (2).svg" alt = "Triangle"/>
-                    <img src= "/Vector (3).svg" alt = "Stairs"/>
+                <img src="/Vector (1).svg" alt="Star" />
+                <img src="/Vector (2).svg" alt="Triangle" />
+                <img src="/Vector (3).svg" alt="Stairs" />
                 </div>
             </div>
-        </div>
+            <div className={Styles.coffeeText}>
+                {t('idea.coffeetext')}
+            </div>
+            <div className={Styles.coffeeButtons}>
+                <div>{t('idea.cobut1')}</div>
+                <div>{t('idea.cobut2')}</div>
+                <div>{t('idea.cobut3')}</div>
+                <div>{t('idea.cobut4')}</div>
+                <div>{t('idea.cobut5')}</div>
+                <div>{t('idea.cobut6')}</div>
+                <div>{t('idea.cobut7')}</div>
+                <div>{t('idea.cobut8')}</div>
+                <div>{t('idea.cobut9')}</div>
+                <div>{t('idea.cobut10')}</div>
+                <div>{t('idea.cobut11')}</div>
+                <div>{t('idea.cobut12')}</div>
+            </div>
+            </div>
+
+            <div className={Styles.ending}>
+            <div className={Styles.endingHead}>{t('idea.partnerhead')}</div>
+            <div className={Styles.endingText}>
+                {t('idea.endingtext')}
+            </div>
+            <div className={Styles.endingYellow}>
+                <div>{t('idea.endingyellow1')}</div>
+                <div>{t('idea.endingyellow2')}</div>
+            </div>
+            <div className={Styles.endingSignup}>
+                {t('idea.signup')}
+            </div>
+            </div>
+
+
+        <FAQ/>
 
         <div className={Styles.partners}>
-            <div className={Styles.past_partners}>Past partners</div>
+            <div className={Styles.past_partners}>{t('idea.partnerhead')}</div>
             <div className={Styles.partner_image}><img src="/image 24.svg" alt="Past Partners"/></div>
-            <div className={Styles.looking_forward}>Looking forward to seeing you in the summer, <br/>MEGA Team</div>
+            <div className={Styles.looking_forward}>{t('idea.looking1')}<br/>{t('idea.looking2')}</div>
         </div>
         <ContactUs/>
     </>
